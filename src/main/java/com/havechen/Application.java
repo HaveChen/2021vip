b4979f18ae9fc33cb2b5d6bf33d29b36d23d047d
HaveChen <1065966570@qq.com>
2021-02-03 10:13:54 +0800

    first

---
+++ src/main/java/com/havechen/Application.java
@@ -0,0 +1,13 @@
+package com.havechen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
